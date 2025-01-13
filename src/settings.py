2c778fe00fff68ddec59e2d4319d5355bd7361a7
NeiroX <stark042004@yandex.ru>
2025-01-13 15:59:13 +0200

    -Changed settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,15 +1,16 @@
 import os
 from urllib.parse import urlparse
-
+from decouple import config
 from dotenv import load_dotenv
 
+# Local environment
 load_dotenv()
 
 # Telegram TOKEN
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TOKEN = config("TELEGRAM_BOT_TOKEN")
 
 # Debug
-DEBUG = os.getenv("DEBUG")
+DEBUG = config("DEBUG")
 # Mongo and Database settings
 # DATABASE_NAME = os.getenv("DATABASE_NAME")
 # DATABASE_URL = os.getenv("DATABASE_URL")
@@ -17,40 +18,39 @@ DEBUG = os.getenv("DEBUG")
 # PosgreSQL database settings
 # Local
 if DEBUG:
-    DATABASE_USER = os.getenv("DATABASE_USER")
-    DATABASE_NAME = os.getenv("DATABASE_NAME")
-    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
-    DATABASE_HOST = os.getenv("DATABASE_HOST")
-    DATABASE_PORT = os.getenv("DATABASE_PORT")
+    DATABASE_USER = config("DATABASE_USER")
+    DATABASE_NAME = config("DATABASE_NAME")
+    DATABASE_PASSWORD = config("DATABASE_PASSWORD")
+    DATABASE_HOST = config("DATABASE_HOST")
+    DATABASE_PORT = config("DATABASE_PORT")
     DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 # Heroku
 else:
-    DATABASE_URL = os.getenv('DATABASE_URL')
+    DATABASE_URL = config('DATABASE_URL')
     if DATABASE_URL:
-        DATABASE_DATA = DATABASE_URL.split(':')
-        DATABASE_USER = DATABASE_DATA[1].replace('//', '')
-        DATABASE_PASSWORD = DATABASE_DATA[2].split('@')[0]
-        DATABASE_HOST = DATABASE_DATA[2].split('@')[1]
-        # DATABASE_PORT = DATABASE_DATA[3].split('/')[0] if DATABASE_DATA[3].split('/')[0] else 5432
-        DATABASE_PORT = "5432"
-        DATABASE_NAME = DATABASE_DATA[3].split('/')[1]
+        parsed_url = urlparse(DATABASE_URL)
+        DATABASE_USER = parsed_url.username
+        DATABASE_PASSWORD = parsed_url.password
+        DATABASE_HOST = parsed_url.hostname
+        DATABASE_PORT = parsed_url.port if parsed_url.port else "5432"  # Default to 5432 if no port is provided
+        DATABASE_NAME = parsed_url.path.lstrip('/')  # Remove the leading '/' from the path
         DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
     else:
         raise ValueError("DATABASE_URL is not set in the environment variables")
 
-AWS_SERVER_PUBLIC_KEY = os.getenv('AWS_SERVER_PUBLIC_KEY')
-AWS_SERVER_SECRET_KEY = os.getenv('AWS_SERVER_SECRET_KEY')
-AWS_REGION = os.getenv('AWS_REGION', 'us-east-1')
+AWS_SERVER_PUBLIC_KEY = config('AWS_SERVER_PUBLIC_KEY')
+AWS_SERVER_SECRET_KEY = config('AWS_SERVER_SECRET_KEY')
+AWS_REGION = config('AWS_REGION', 'us-east-1')
 BUCKET_NAME = None
-if os.getenv('BUCKET_NAME', '').strip():
-    BUCKET_NAME = os.getenv('BUCKET_NAME')
+if config('BUCKET_NAME', '').strip():
+    BUCKET_NAME = config('BUCKET_NAME')
 ENDPOINT_URL = None
-if os.getenv('ENDPOINT_URL', '').strip():
-    ENDPOINT_URL = os.getenv('ENDPOINT_URL')
+if config('ENDPOINT_URL', '').strip():
+    ENDPOINT_URL = config('ENDPOINT_URL')
 EDGE_ENDPOINT_URL = None
-if os.getenv('EDGE_ENDPOINT_URL', '').strip():
-    EDGE_ENDPOINT_URL = os.getenv('EDGE_ENDPOINT_URL')
+if config('EDGE_ENDPOINT_URL', '').strip():
+    EDGE_ENDPOINT_URL = config('EDGE_ENDPOINT_URL')
 CUSTOM_ENDPOINT_URL = None
-if os.getenv('CUSTOM_ENDPOINT_URL', '').strip():
-    CUSTOM_ENDPOINT_URL = os.getenv('CUSTOM_ENDPOINT_URL')
+if config('CUSTOM_ENDPOINT_URL', '').strip():
+    CUSTOM_ENDPOINT_URL = config('CUSTOM_ENDPOINT_URL')
