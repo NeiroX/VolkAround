a422d9ce0fae8e4b23615fd28fecf4458fb06fe4
NeiroX <stark042004@yandex.ru>
2025-01-13 15:19:50 +0200

    -Updated section default parameters

--- alembic/env.py
+++ alembic/env.py
@@ -66,14 +66,7 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {
-            "port": DATABASE_PORT,
-            "name": DATABASE_NAME,
-            "host": DATABASE_HOST,
-            "user": DATABASE_USER,
-            "password": DATABASE_PASSWORD,
-            "url": DATABASE_URL,
-        }),
+        config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
