3108ce0a13d4956463b97505b7594ae06bb41cca
NeiroX <stark042004@yandex.ru>
2025-01-13 16:08:37 +0200

    -Added default values in settings.py

--- src/settings.py
+++ src/settings.py
@@ -18,16 +18,16 @@ DEBUG = config("DEBUG")
 # PosgreSQL database settings
 # Local
 if DEBUG:
-    DATABASE_USER = config("DATABASE_USER")
-    DATABASE_NAME = config("DATABASE_NAME")
-    DATABASE_PASSWORD = config("DATABASE_PASSWORD")
-    DATABASE_HOST = config("DATABASE_HOST")
-    DATABASE_PORT = config("DATABASE_PORT")
+    DATABASE_USER = config("DATABASE_USER", default=None, cast=str)
+    DATABASE_NAME = config("DATABASE_NAME", default=None, cast=str)
+    DATABASE_PASSWORD = config("DATABASE_PASSWORD", default=None, cast=str)
+    DATABASE_HOST = config("DATABASE_HOST", default=None, cast=str)
+    DATABASE_PORT = config("DATABASE_PORT", default=None, cast=int)
     DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 # Heroku
 else:
-    DATABASE_URL = config('DATABASE_URL')
+    DATABASE_URL = config('DATABASE_URL', default=None, cast=str)
     if DATABASE_URL:
         parsed_url = urlparse(DATABASE_URL)
         DATABASE_USER = parsed_url.username
@@ -43,14 +43,14 @@ AWS_SERVER_PUBLIC_KEY = config('AWS_ACCESS_KEY_ID', default=None, cast=str)
 AWS_SERVER_SECRET_KEY = config('AWS_SECRET_ACCESS_KEY', default=None, cast=str)
 AWS_REGION = config('AWS_REGION', default='eu-central-1')
 BUCKET_NAME = None
-if config('BUCKET_NAME', '').strip():
+if config('BUCKET_NAME', default='').strip():
     BUCKET_NAME = config('BUCKET_NAME')
 ENDPOINT_URL = None
-if config('ENDPOINT_URL', '').strip():
+if config('ENDPOINT_URL', default='').strip():
     ENDPOINT_URL = config('ENDPOINT_URL')
 EDGE_ENDPOINT_URL = None
-if config('EDGE_ENDPOINT_URL', '').strip():
+if config('EDGE_ENDPOINT_URL', default='').strip():
     EDGE_ENDPOINT_URL = config('EDGE_ENDPOINT_URL')
 CUSTOM_ENDPOINT_URL = None
-if config('CUSTOM_ENDPOINT_URL', '').strip():
+if config('CUSTOM_ENDPOINT_URL', default='').strip():
     CUSTOM_ENDPOINT_URL = config('CUSTOM_ENDPOINT_URL')
