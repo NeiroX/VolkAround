596664d4822987176826295b528471e42b659226
NeiroX <stark042004@yandex.ru>
2025-01-13 15:35:39 +0200

    -Added logging

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import os
 from alembic import command
 from alembic.config import Config
 from sqlalchemy.orm import Session
-
+import logging
 from src.components.messages.bot import Bot
 from src.database.session import create_session
 from src.settings import TOKEN, DATABASE_URL
@@ -19,10 +19,15 @@ def apply_migrations():
     # Construct the absolute path to the alembic.ini file
     alembic_ini_path = os.path.join(cwd, "alembic.ini")
     alembic_cfg = Config(alembic_ini_path)  # Path to your Alembic configuration file
+    logging.debug(f"Alembic ini path: {alembic_ini_path}")
     alembic_cfg.set_main_option('sqlalchemy.url', DATABASE_URL)
+    logging.debug(f"Set sqlalchemy.url parameter to: {DATABASE_URL}")
+    logging.debug(f"Current sqlalchemy.url: {alembic_cfg.get_main_option('sqlalchemy.url')}")
+
     command.upgrade(alembic_cfg, "head")  # Apply all migrations up to the latest
 
 def test_connection():
+    logging.debug("Testing database connection")
     conn = get_db_connection()
     if conn:
         try:
@@ -35,11 +40,24 @@ def test_connection():
 
 
 if __name__ == "__main__":
+
+    # Configure the logging system
+    logging.basicConfig(
+        level=logging.DEBUG,  # Log everything from DEBUG level and above
+        format="%(asctime)s [%(levelname)s] %(message)s",
+    )
+
+    # Example log messages
+    logging.debug("Running main file!!!")
+
     if not TOKEN:
         raise ValueError("Missing TELEGRAM_BOT_TOKEN in .env file")  # Replace with your bot token
     # Test connection to database and apply migrations
     test_connection()
     apply_migrations()
+
+
+
 
     # Start bot
     session: Session = create_session()
