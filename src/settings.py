1b6b49922b1c8b3edeb0b7d890d058458e24feff
NeiroX <stark042004@yandex.ru>
2025-01-13 12:08:57 +0200

    -Changed alembic files

--- src/settings.py
+++ src/settings.py
@@ -17,9 +17,12 @@ DEBUG = os.getenv("DEBUG")
 # PosgreSQL database settings
 if DEBUG:
     DATABASE_USER = os.getenv("DATABASE_USER")
+    DATABASE_NAME = os.getenv("DATABASE_NAME")
     DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
     DATABASE_HOST = os.getenv("DATABASE_HOST")
     DATABASE_PORT = os.getenv("DATABASE_PORT")
+    DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+
 else:
     DATABASE_URL = os.getenv("DATABASE_URL")
     if DATABASE_URL:
